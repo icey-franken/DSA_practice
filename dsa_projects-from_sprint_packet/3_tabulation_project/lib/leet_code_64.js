84ee05adc96544f25f37c57df5add9ec357b8df5
Isaac <finken.isaac@gmail.com>
2020-12-29 18:06:14 -0600

    Set up leetcode 64 and 70 in project 3.

--- dsa_projects-from_sprint_packet/3_tabulation_project/lib/leet_code_64.js
+++ dsa_projects-from_sprint_packet/3_tabulation_project/lib/leet_code_64.js
@@ -1,6 +1,32 @@
 // Work through this problem on https://leetcode.com/problems/minimum-path-sum/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function minPathSum(grid) {
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+// Note: You can only move either down or right at any point in time.
 
-}
+// Example 1:
+// Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+// Output: 7
+// Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+// Example 2:
+// Input: grid = [[1,2,3],[4,5,6]]
+// Output: 12
+
+// Constraints:
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 200
+// 0 <= grid[i][j] <= 100
+
+// 1. clarify/test I/O/edge cases:
+// 2. formulate approach
+// 3. pseudocode
+// 4. code
+function minPathSum(grid) {}
+
+// 5. example input
+console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]])) // 7
+console.log(minPathSum([[1,2,3],[4,5,6]])) // 12
+
+// 6. time/space complexity
