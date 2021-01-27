e7cc5532b0db13808b59bfa2ed78bd2d3f674ade
Isaac <finken.isaac@gmail.com>
2021-01-27 16:14:43 -0600

    Completed Project 6. Passing 4/4 (binarySearchIndex).

--- dsa_projects-from_sprint_packet/6_binary_search_project/lib/binary_search.js
+++ dsa_projects-from_sprint_packet/6_binary_search_project/lib/binary_search.js
@@ -15,7 +15,22 @@ function binarySearch(array, target) {
   return false;
 }
 
-function binarySearchIndex(array, target) {}
+function binarySearchIndex(array, target) {
+	let left = 0;
+  let right = array.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midVal = array[mid];
+    if (midVal === target) {
+      return mid;
+    } else if (midVal > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+}
 
 module.exports = {
   binarySearch,
